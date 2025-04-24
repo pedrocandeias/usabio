af88281190053962bbcfa608112d671273d684e9
Pedro Candeias <pedrocandeias@gmail.com>
2025-04-24 11:12:26 +0100

    Added tools like gulp, yarn, npm, etc.

---
+++ tools/gulpfile.js
@@ -0,0 +1,29 @@
+import { cleanTask } from "./gulp/clean.js";
+import {
+  localHostTask,
+  reloadTask,
+  watchTask,
+  watchSCSSTask,
+  watchJSTask,
+} from "./gulp/watch.js";
+import {
+  // rtlTask,
+  // buildBundleTask,
+  compileTask,
+} from "./gulp/compile.js";
+
+// Clean tasks:
+export { cleanTask as clean };
+
+// Watch tasks:
+export { localHostTask as localhost };
+export { reloadTask as reload };
+export { watchTask as watch };
+export { watchSCSSTask as watchSCSS };
+export { watchJSTask as watchJS };
+
+// Main tasks:
+export { compileTask as compile };
+
+// Entry point:
+export default compileTask;
